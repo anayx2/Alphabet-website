b9d5eddd10538ba407037929bfa86ccdf3568150
anayx2 <anay@alphabettechnology.in>
2024-12-16 17:52:42 +0530

    form update

--- src/components/Homepage/Contact.js
+++ src/components/Homepage/Contact.js
@@ -59,9 +59,9 @@ const Contact = () => {
               <div className="max-w-4xl mx-auto px-4 py-8">
                 <form
                   onSubmit={handleSubmit}
-                  className="bg-white rounded-lg p-6 shadow-lg"
+                  className="bg-white rounded-lg p-6"
                 >
-                  <h2 className="text-3xl font-bold text-gray-900 mb-6">
+                  <h2 className="text-4xl font-bold text-gray-900 mb-10">
                     Ready to discuss <br /> your project with us?
                   </h2>
 
